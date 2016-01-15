dfe0c8c86b298d58fa28cf3489645d7d7facc43f
Marika Allely <mballely@gmail.com>
2016-01-15 09:08:18 -0800

    add css styling

---
+++ script.js
@@ -0,0 +1,9 @@
+$(function) {
+  $("form#survey").submit(function(event) {
+    var climate = $("select#climate").val();
+    var region = $("select#region").val();
+    var terrain = $("select#terrain").val();
+    var drink = $("select#drink").val();
+    var food = $("select#food").val();
+  })
+}
